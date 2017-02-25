797bd5317a328ea07d94e5bb9ed95ee4f6c68c10
robot <cylehunter33@gmail.com>
2017-02-24 21:24:14 -0600

    First commit

---
+++ Gemfile
@@ -0,0 +1,7 @@
+source 'https://rubygems.org'
+
+gem "berkshelf"
+gem 'chef', '~> 12.6.0'
+gem "test-kitchen"
+gem "kitchen-vagrant"
+gem "kitchen-docker"
--- metadata.rb
+++ metadata.rb
@@ -17,3 +17,4 @@ version '0.1.0'
 # a Supermarket.
 #
 # source_url 'https://github.com/<insert_org_here>/playground' if respond_to?(:source_url)
+depends 'docker', '~> 2.1'
